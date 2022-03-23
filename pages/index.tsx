ceb221f7177b4fcbe011593037a277e915f06a4f
Frank Lan <frank.lan@rea-group.com>
2022-03-23 16:47:03 +1100

    use `next/image`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <main>
@@ -49,14 +50,15 @@ export default function Home() {
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+        <span>Powered by&nbsp;</span>
+        <div className="logo">
+          <Image
+            alt='Vercel'
+            src='/vercel.svg'
+            layout='fill'
+            objectFit='contain'
+          />
+        </div>
       </footer>
 
       <style jsx>{`
@@ -135,7 +137,7 @@ export default function Home() {
           padding: 0.75rem;
           font-size: 1.1rem;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
 
         .grid {
@@ -179,7 +181,9 @@ export default function Home() {
         }
 
         .logo {
-          height: 1em;
+          height: 3em;
+          padding-left: 3em;
+          position: relative;
         }
 
         @media (max-width: 600px) {
@@ -196,8 +200,8 @@ export default function Home() {
           padding: 0;
           margin: 0;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
+          Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+          sans-serif;
         }
 
         * {
