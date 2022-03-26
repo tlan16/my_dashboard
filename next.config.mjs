c327cf57863295f77b77376e10a7c1e9f5d23748
Frank Lan (Bot) <franklan118@gmail.com>
2022-03-26 19:38:01 +1100

    fix basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+const basePath = process.env.NODE_ENV === 'production' ? '/my_dashboard' : '';
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -5,6 +7,8 @@ const nextConfig = {
   generateBuildId: async () => {
     return new Date().toJSON()
   },
+  basePath,
+  assetPrefix: `${basePath}/`
 }
 
 export default nextConfig
